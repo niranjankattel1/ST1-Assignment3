70e84972e1f0fb624820f28f3bd428641f45cd5a
Niranjan Kattel <u3319942@uni.canberra.edu.au>
2026-04-29 18:53:49 +1000

    Add loadimage

---
+++ utils/kaggleHub.py
@@ -0,0 +1,7 @@
+import kagglehub
+from config import RAW_DATA_DIR
+
+def download():
+    # Download latest version
+    path = kagglehub.dataset_download("kennethtm/stream-macroinvertebrates", output_dir=RAW_DATA_DIR)
+    print("Path to dataset files:", path)
