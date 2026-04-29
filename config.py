67e7f74c9f53fbcf3ebc750f3f91f0a83c1d331f
Niranjan Kattel <u3319942@uni.canberra.edu.au>
2026-04-29 18:57:20 +1000

    Add loadimage

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 OUTPUTS_DIR = BASE_DIR / "outputs"
